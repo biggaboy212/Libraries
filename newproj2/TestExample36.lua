d647e67603d2aa6bafee84d59db14bdbedc4c5cc
biggaboy212 <75142294+biggaboy212@users.noreply.github.com>
2024-03-31 22:46:43 -0500

    Update TestExample36.lua

--- newproj2/TestExample36.lua
+++ newproj2/TestExample36.lua
@@ -1,5 +1,5 @@
 --// Variables
-local Version = '36'
+local Version = '37'
 local HttpService = game:GetService("HttpService")
 local players = game:GetService("Players")
 local localPlayer = players.LocalPlayer
@@ -47,8 +47,8 @@ Combat:NewKeybind("InvisKill Keybind", Enum.KeyCode.Unknown, function(key)
     local bp = backpack
     local mouse = localPlayer:GetMouse()
     UIS.InputBegan:Connect(function(Input, GameProcessedEvent)
-        print('input began: '.. tostring(Input.KeyCode) ..' | KB Input: '.. tostring(key))
-        if Input.KeyCode == Input.KeyCode[key] and not GameProcessedEvent then
+        print('input began: '.. tostring(Input.KeyCode) ..' | KB Input: '.. tostring(Input.KeyCode[key]))
+        if tostring(Input.KeyCode) == tostring(Input.KeyCode[key]) then
             print('right key') 
             if mouse.Target then
             ScriptVariables.OriginalPosition = hum.CFrame
